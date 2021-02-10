e78ec0fe48c6d0782ecbcae57c5edd33ecf4d143
Andy Doan <andy@foundries.io>
2021-02-09 22:49:42 -0600

    Add CLI parser
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 
-int main(int argc, char **argv) { std::cout << "hello\n"; }
+#include "CLI11.hpp"
+
+int main(int argc, char **argv) {
+  CLI::App app{"capp-run"};
+
+  CLI11_PARSE(app, argc, argv);
+
+  std::cout << "hello\n";
+  return 0;
+}
