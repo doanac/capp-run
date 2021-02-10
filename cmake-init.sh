a4309ffde02631201903f7c1388a61e77899179c
Andy Doan <andy@foundries.io>
2021-02-09 22:34:57 -0600

    add skeleton
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ cmake-init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+## Just an easy way to get cmake/ninja set up so you don't have to remember
+
+HERE=$(dirname $(readlink -f $0))
+
+if [ "$HERE" = "$(pwd)" ] ; then
+	echo "ERROR: Don't run this from source directory."
+	exit 1
+fi
+
+CMAKE_DEF_ARGS="-GNinja"
+CMAKE_ARGS="${CMAKE_DEF_ARGS} $@"
+echo "Building capp-run with the following arguments: ${CMAKE_ARGS}"
+cmake ${CMAKE_ARGS} $HERE
